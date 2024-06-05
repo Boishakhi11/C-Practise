226794fcc65879ec974c39909e767949e33d462a
Boishakhi Ghosh Mukta <141757309+Boishakhi11@users.noreply.github.com>
2024-06-05 21:40:31 +0000

    problem solved

---
+++ codeforces.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main() {
+    string s;
+    cout<< "Enter your name: ";
+    cin>> s;
+    cout<< "Hello, "<< s;
+    return 0;
+};
