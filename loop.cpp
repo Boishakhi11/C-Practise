da12537d31a607c3c7d978f4d5ae09779df17336
Boishakhi Ghosh Mukta <141757309+Boishakhi11@users.noreply.github.com>
2024-06-06 21:33:51 +0000

    loop

---
+++ loop.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main () {
+    int i = 0;
+    do {
+        cout << i <<endl;
+        i ++;
+    }
+
+    while (i < 7);
+    /*while (i < 5) {
+        cout << i << endl ;
+        i ++ ;
+    }
+
+    for (int x = 5; x < 11 ; x ++) {
+        cout << x << endl ;
+    } */
+    return 0;
+};
